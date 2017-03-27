3f02bd509600b10307ab0396a877b8987aa0e4fb
Convery <convery10122@gmail.com>
2017-03-28 01:25:41 +0200

    Added a hooking utility.

--- Source/StdInclude.h
+++ Source/StdInclude.h
@@ -15,6 +15,7 @@
 #include "Utilities/Text/Variadicstring.h"
 #include "Utilities/Binary/Memprotect.h"
 #include "Utilities/Data/Bytebuffer.h"
+#include "Utilities/Binary/Hooking.h"
 #include "Utilities/Text/Logfile.h"
 #include "Utilities/Crypto/FNV1.h"
 
