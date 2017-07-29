22a64e4049d1e54939d87082e0a4e46db250e898
Convery <convery@localhost.localdomain>
2017-07-29 16:47:55 +0200

    Added the base header and helpers.

---
+++ Source/Stdinclude.h
@@ -0,0 +1,26 @@
+/*
+    Initial author: Convery (tcn@ayria.se)
+    Started: 29-07-2017
+    License: MIT
+    Notes:
+*/
+
+#pragma once
+
+// Includes for configuration settings.
+#include "Configuration/Defines.h"
+
+// Includes for standard libraries.
+#include <cstdint>
+#include <cstdlib>
+#include <cstdarg>
+#include <cstring>
+
+// Includes for thirdparty libraries.
+/* ... */
+
+// Includes for our utilities.
+/* ... */
+
+// Includes for our components.
+/* ... */
