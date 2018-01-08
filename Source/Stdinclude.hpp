10b326594f8f2ef3413c2114911a1c038b11ccc2
Convery <convery10122@gmail.com>
2018-01-08 03:37:36 +0100

    Added bytebuffer.

--- Source/Stdinclude.hpp
+++ Source/Stdinclude.hpp
@@ -47,6 +47,7 @@
 #include "Utility/Variadicstring.hpp"
 #include "Utility/Filesystem.hpp"
 #include "Utility/Memprotect.hpp"
+#include "Utility/Bytebuffer.hpp"
 #include "Utility/FNV1Hash.hpp"
 #include "Utility/Logfile.hpp"
 #include "Utility/Base64.hpp"
