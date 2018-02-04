e4985556c3d0da102e736bb8fe159399ca970690
Convery <Convery10122@gmail.com>
2018-02-04 14:58:12 +0100

    Comment fix.

--- Source/Appmain.cpp
+++ Source/Appmain.cpp
@@ -42,7 +42,7 @@ extern "C" EXPORT_ATTR void onMessage(uint32_t MessageID, uint32_t Messagesize, 
         ----------------------------------------------------------------------
     */
 
-    // Create a buffer from the data as such:
+    // Create a buffer from the data like such:
     // Bytebuffer Message{ Messagesize, Messagedata};
 
     // MessageID is a FNV1a_32 hash of a string.
